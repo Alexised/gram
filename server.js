4ac85a8db2fe5db3c21a1c00335faa656f56f718
Alexis Rodriguez <edwinalexis1996@hotmail.com>
2019-02-18 16:07:29 -0500

    actualizacion

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require("express");
+
+var app = express();
+
+app.get('/', function(req,res){
+    res.send('hola');
+});
+
+app.listen(3000,function(err){
+    if(err) return console.log("hubo un error"), process.exit(1);
+    console.log("escuchando en el puerto 3000");
+});
