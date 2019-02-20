13d558bb66606fb0c83cf3b93c661c96132865dc
Alexis Rodriguez <edwinalexis1996@hotmail.com>
2019-02-20 09:48:22 -0500

    secrea server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require("express");
 var app = express();
 
 app.get('/', function(req,res){
-    res.send('hola');
+    res.send('servidor activo ');
 });
 
 app.listen(3000,function(err){
