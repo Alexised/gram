37fe84fd857df9e35468d8123026627c5254fbe9
zekken25 <edwinalexis1996@hotmail.com>
2019-02-21 14:04:42 -0500

    se agrega babel

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,10 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var rename = require('gulp-rename')
+var rename = require('gulp-rename');
+var babel = require('babelify');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+
 gulp.task('styles',function(){
     gulp
         .src('index.scss')
@@ -9,4 +13,18 @@ gulp.task('styles',function(){
         .pipe(gulp.dest('public'));
 })
 
-gulp.task('default',['styles'])
+gulp.task('assets',function(){
+    gulp
+      .src('assets/*')
+      .pipe(gulp.dest('public'));
+})
+gulp.task('scripts',function(){ 
+    browserify('./src/index.js')
+    .transform(babel)
+    .bundle()
+    .pipe(source('index.js'))
+    .pipe(rename('app.js'))
+    .pipe(gulp.dest('public'))
+})
+
+gulp.task('default',['styles','assets','scripts'])
--- server.js
+++ server.js
@@ -2,8 +2,10 @@ var express = require("express");
 
 var app = express();
 app.set('view engine','pug');
+app.use(express.static('public'));
+
 app.get('/', function(req,res){
-    res.render('index')
+    res.render('index');
 });
 
 app.listen(3000,function(err){
