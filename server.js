4573d21a219e5f9a091e391895e14554742f982e
zekken25 <edwinalexis1996@hotmail.com>
2019-02-21 15:43:50 -0500

    se agrega page

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ app.get('/', function(req,res){
     res.render('index');
 });
 
+app.get('/signup', function(req,res){
+    res.render('index');
+});
+
+app.get('/signin', function(req,res){
+    res.render('index');
+});
+
 app.listen(3000,function(err){
     if(err) return console.log("hubo un error"), process.exit(1);
     console.log("escuchando en el puerto 3000");
--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-alert('hola');
+var page = require('page');
+
+
+var main =document.getElementById('main-container')
+
+page('/',function(){
+main.innerHTML='home <a href="/signup">signup</a>'
+})
+
+page('/signup',function(ctx, next){
+main.innerHTML='signup <a href="/">Home</a>'
+    
+})
+
+page();
