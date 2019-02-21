1f90cf799a6ef9bc22f365f86d515b43a3685c61
zekken25 <edwinalexis1996@hotmail.com>
2019-02-21 17:16:02 -0500

    se agrega watch

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,6 +4,7 @@ var rename = require('gulp-rename');
 var babel = require('babelify');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var watchify = require('watchify');
 
 gulp.task('styles',function(){
     gulp
@@ -18,13 +19,25 @@ gulp.task('assets',function(){
       .src('assets/*')
       .pipe(gulp.dest('public'));
 })
-gulp.task('scripts',function(){ 
-    browserify('./src/index.js')
-    .transform(babel)
-    .bundle()
-    .pipe(source('index.js'))
-    .pipe(rename('app.js'))
-    .pipe(gulp.dest('public'))
-})
 
-gulp.task('default',['styles','assets','scripts'])
+function compile(watch){
+    var bundle = watchify(browserify('./src/index.js'));
+    function rebundel(){
+        bundle
+            .transform(babel)
+            .bundle()
+            .pipe(source('index.js'))
+            .pipe(rename('app.js'))
+            .pipe(gulp.dest('public'));
+    }
+    if(watch){
+        bundle.on('update',function(){
+            console.log('--> Bundling...')
+            rebundel();
+        })
+    }
+    rebundel();
+}   
+gulp.task('build',function(){return compile();});
+gulp.task('watch',function(){return compile(true);});
+gulp.task('default',['styles','assets','build'])
